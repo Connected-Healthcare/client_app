162576b75c7b8b5691452879785682f7b867024b
Niket Naidu <coder137@gmail.com>
2021-03-13 12:58:13 -0800

    Updated realtime screen
    - Get bluetooth data from microcontroller and render as per schema

--- client_app/lib/bt/screen/realtime.dart
+++ client_app/lib/bt/screen/realtime.dart
@@ -11,7 +11,7 @@ class BluetoothRealtimeScreen extends StatefulWidget {
 
 class _BluetoothRealtimeScreenState extends State<BluetoothRealtimeScreen> {
   static const int MAXSIZE = 20;
-  List<String> _informations = List<String>();
+  List<String> _informations = [];
 
   @override
   Widget build(BuildContext context) {
@@ -55,11 +55,8 @@ class _BluetoothRealtimeScreenState extends State<BluetoothRealtimeScreen> {
         return ListView.builder(
           itemCount: _informations.length,
           itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                title: Text("${_informations[index]}"),
-              ),
-            );
+            return BluetoothInformationSchemaCard(
+                _informations[index].trim().split(","));
           },
         );
       },
@@ -83,5 +80,91 @@ class _BluetoothRealtimeScreenState extends State<BluetoothRealtimeScreen> {
       _informations.removeLast();
     }
     _informations.insert(0, line);
+  }
+}
+
+// * NOTE, Make sure this InformationSchema represents what is being sent from the microcontroller
+// See https://github.com/Connected-Healthcare/b_l475e_iot01a_sensors/blob/main/HelloWorld_ST_Sensors/main.cpp
+// Line 146 on Date 13 March 2021
+// https://github.com/Connected-Healthcare/b_l475e_iot01a_sensors/pull/1
+class BluetoothInformationSchemaCard extends StatelessWidget {
+  final List<String> tokens;
+  BluetoothInformationSchemaCard(this.tokens);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          // HTS221
+          _createListTile(
+            "HTS221",
+            [
+              "Humidity: ${tokens[1]}",
+              "Temperature: ${tokens[0]} C",
+            ],
+          ),
+
+          // LPS22HB
+          _createListTile(
+            "LPS22HB",
+            [
+              "Pressure: ${tokens[3]} mbar",
+              "Temperature: ${tokens[2]} C",
+            ],
+          ),
+
+          // Magnetometer
+          _createListTile(
+            "LIS3MDL Magnetometer (mag/mgauss)",
+            [
+              "X: ${tokens[4]} Y: ${tokens[5]} Z: ${tokens[6]}",
+            ],
+          ),
+
+          // Accelerometer
+          _createListTile(
+            "LSM6DSL Accelerometer (acc/mg)",
+            [
+              "X: ${tokens[7]} Y: ${tokens[8]} Z: ${tokens[9]}",
+            ],
+          ),
+
+          // Gyroscope
+          _createListTile(
+            "LSM6DSL Gyroscope (gyro/mdps)",
+            [
+              "X: ${tokens[10]} Y: ${tokens[11]} Z: ${tokens[12]}",
+            ],
+          ),
+
+          // Time of Flight
+          _createListTile(
+            "VL53L0X Time of Flight",
+            [
+              "Distance: ${tokens[13]} mm",
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _createListTile(String title, List<String> subtitles) {
+    return ListTile(
+      isThreeLine: subtitles.length == 1 ? false : true,
+      title: Text(title),
+      // subtitle: Text(subtitles[0]),
+      subtitle: subtitles.length == 1
+          ? Text(subtitles[0])
+          : Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                for (String subtitle in subtitles) Text(subtitle),
+              ],
+            ),
+      // Highlight when selected
+      onTap: () {},
+    );
   }
 }
