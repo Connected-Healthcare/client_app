152f3dda3f035af514a3b76b3a4bd23e141a78fc
Niket Naidu <coder137@gmail.com>
2021-05-02 22:05:35 -0700

    Added 5 customizable buttons

--- client_app/lib/storage/shared_prefs_model.dart
+++ client_app/lib/storage/shared_prefs_model.dart
@@ -7,6 +7,11 @@ class SharedPrefsModel extends ChangeNotifier {
     _writeInitialString(BLUETOOTH_ADDRESS_KEY, "");
     _writeInitialString(BLUETOOTH_NAME_KEY, "");
     _writeInitialString(USER_IDENTIFIER_KEY, "");
+    _writeInitialString(BUTTON_B1, "");
+    _writeInitialString(BUTTON_B2, "");
+    _writeInitialString(BUTTON_B3, "");
+    _writeInitialString(BUTTON_B4, "");
+    _writeInitialString(BUTTON_B5, "");
   }
 
   void _writeInitialString(String key, String initialData) {
@@ -37,4 +42,42 @@ class SharedPrefsModel extends ChangeNotifier {
   }
 
   String get userIdentifier => prefs.getString(USER_IDENTIFIER_KEY);
+
+  // Buttons
+  static const String BUTTON_B1 = "_buttonB1";
+  static const String BUTTON_B2 = "_buttonB2";
+  static const String BUTTON_B3 = "_buttonB3";
+  static const String BUTTON_B4 = "_buttonB4";
+  static const String BUTTON_B5 = "_buttonB5";
+
+  Future<void> setButton1(String info) async {
+    await prefs.setString(BUTTON_B1, info);
+    notifyListeners();
+  }
+
+  Future<void> setButton2(String info) async {
+    await prefs.setString(BUTTON_B2, info);
+    notifyListeners();
+  }
+
+  Future<void> setButton3(String info) async {
+    await prefs.setString(BUTTON_B3, info);
+    notifyListeners();
+  }
+
+  Future<void> setButton4(String info) async {
+    await prefs.setString(BUTTON_B4, info);
+    notifyListeners();
+  }
+
+  Future<void> setButton5(String info) async {
+    await prefs.setString(BUTTON_B5, info);
+    notifyListeners();
+  }
+
+  String get button1 => prefs.getString(BUTTON_B1);
+  String get button2 => prefs.getString(BUTTON_B2);
+  String get button3 => prefs.getString(BUTTON_B3);
+  String get button4 => prefs.getString(BUTTON_B4);
+  String get button5 => prefs.getString(BUTTON_B5);
 }
